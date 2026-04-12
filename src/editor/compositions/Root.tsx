35a08375b0516fb6bb00d411b4d8a02fa116afb7
yonghyeon-dev <kryou1@naver.com>
2026-04-12 23:16:15 +0900

    WI-030-036-feat 편집/합성 Remotion 모듈 구현 (#10)
    Co-authored-by: yonghyeon-dev <cyh@flow-coder.com>

---
+++ src/editor/compositions/Root.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Composition } from "remotion";
+import { Drama, type DramaProps } from "./Drama";
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <>
+      <Composition
+        id="DramaShorts"
+        component={Drama}
+        durationInFrames={1800}
+        fps={30}
+        width={1080}
+        height={1920}
+        defaultProps={{
+          specPath: "",
+          assetsDir: "",
+          format: "shorts",
+        }}
+      />
+      <Composition
+        id="DramaLongform"
+        component={Drama}
+        durationInFrames={27000}
+        fps={30}
+        width={1920}
+        height={1080}
+        defaultProps={{
+          specPath: "",
+          assetsDir: "",
+          format: "longform",
+        }}
+      />
+    </>
+  );
+};
